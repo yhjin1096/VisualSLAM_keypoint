0a00b11bae5d4f91ae3e85b4f91b6c55486c6b3c
root <yhjin1096@gmail.com>
2023-10-06 14:24:37 +0900

    Init VPS workspace

---
+++ src/VPS/src/main.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <ceres/ceres.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "hello" << std::endl;
+    
+    return 0;
+}
